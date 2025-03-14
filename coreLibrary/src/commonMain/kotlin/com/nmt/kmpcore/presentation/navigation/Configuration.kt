59701f473f0e37eef0ac16cab9567b1c79d09894
TuNguyen <nmtu2272000@gmail.com>
2025-03-15 00:09:14 +0700

    update navigation

--- coreLibrary/src/commonMain/kotlin/com/nmt/kmpcore/presentation/navigation/Configuration.kt
+++ coreLibrary/src/commonMain/kotlin/com/nmt/kmpcore/presentation/navigation/Configuration.kt
@@ -3,7 +3,4 @@ package com.nmt.kmpcore.presentation.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface Configuration {
-    @Serializable
-    data object HomeScreen : Configuration
-}
+open class Configuration
